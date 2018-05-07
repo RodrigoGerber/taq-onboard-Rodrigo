39b2bf73d00f434fa453bd9073fe83a9e3c750e9
“rodrigogerber” <rodrigo.gerber@taqtile.com>
2018-05-07 14:15:23 -0300

    login screen with local input validation

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -11,28 +11,53 @@ import {
   Text,
   View
 } from 'react-native';
+import Button from './src/components/Button'
+import Input from './src/components/Input'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class App extends Component {
+  state = {
+    email: '',
+    password: ''
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  validate() {
+    var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(!regex.test(this.state.email)) {
+      console.log('erro_email');
+    }
+    if(this.state.password.length < 4) {
+      console.log('erro_senha');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          This is my first React-Native App!
-        </Text>
-        <Text style={styles.instructions}>
-          We S2 Apps
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.headerView}>
+          <Text style={styles.headerText}>
+            MyFirstApp
+          </Text>
+        </View>
+        <View>
+          <Input 
+            placeholder='me@example.com'
+            text='Email'
+            secureTextEntry={false}
+            onChangeText={(text) => this.setState({email: text})}
+          />
+          <Input
+            placeholder='password'
+            text='Password'
+            secureTextEntry={true}
+            onChangeText={(text) => this.setState({password: text})}
+          />
+          <Button
+            text='Login'
+            onPress={() => {
+              this.validate();
+            }}
+          />
+        </View>
       </View>
     );
   }
@@ -40,19 +65,20 @@ export default class App extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
+    backgroundColor: '#ff8080',
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  headerView: {
+    marginBottom: 20,
+    paddingTop: 15,
+    elevation: 2
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  headerText: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#000'
+  }
 });
